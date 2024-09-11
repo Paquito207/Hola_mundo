e6f78f5128e4b38736f38bba3a40f80287c644ea
Paquito207 <robert_cachay@soy.sena.edu.co>
2024-09-11 16:45:40 -0500

    se agrego un archivo nuevo prueba2.py

--- prueba.py
+++ prueba.py
@@ -1,6 +1,16 @@
+import time
 
+print("hola")
+#esta linea imprime el comentario hola
 print ("Hello World! \n ")
-
+time.sleep(4)
 print("segundo reglon")
-
-
+'''tipo de dato
+dfdf
+d
+df
+df
+'''
+print(type("hola"))#la founcion type me dice que tipo de datos es lo que esta en los parentisis
+print(type(4))
+print(type(5.5))
