d7f7de95b135ad5372cfd5f8c2381bf4f65ee720
Paquito207 <robert_cachay@soy.sena.edu.co>
2024-09-18 17:25:15 -0500

    actualizacion tres

--- ejerciciosfor.py
+++ ejerciciosfor.py
@@ -27,5 +27,72 @@ print(f"Tienes {edad} años.")
 def imprimir_años(edad):
     for año in range(1, edad + 1):
         print(f" cantidad de años {año}")
-#función para imprimir los años
+función para imprimir los años
 imprimir_años(edad)
+
+
+#range (inicio, fin, salto):                                                numero+1
+def numeros_impares():
+    numero= int(input("ingrese el numero"))
+
+    for i in range(1, numero+1,2):
+        print(i, end=",")
+numeros_impares()
+
+
+
+
+#brake rompe el ciclo for
+#ejercicio
+def numeros_impares():
+    numero= int(input("ingrese el numero"))
+
+    for i in range(1, numero+1,2):
+        print(i)
+        if i == 15:
+            break;
+numeros_impares()
+
+
+
+
+#ejercicio segundos
+def reloj():
+    #declaracion de la variable, su tipo, y mensaje
+    numero= int(input("ingrese el limite de segundos"))
+    for i in range(1, numero,1):
+        print(i, "segundos")
+        time.sleep(1)
+        if i == numero:
+            print("tiempo maximo")
+            break;
+reloj()
+
+
+
+
+#ejercicio
+
+inversion_inicial = float(input("Ingrese la cantidad de inversión inicial: "))
+porcentaje_interes = float(input("Ingrese el porcentaje de interés anual: "))
+tasa_interes = porcentaje_interes / 100
+anios = int(input("Ingrese el número de años: "))
+for anio in range(1, anios + 1):
+    ganancia_anual = inversion_inicial * tasa_interes
+    inversion_inicial += ganancia_anual
+    print(f"Año {anio}: Ganancia = {ganancia_anual:.2f}, Total = {inversion_inicial:.2f}")
+
+def imprimir_piramide(n):
+    for i in range(n):
+        # Imprimir espacios
+        for j in range(n - i - 1):
+            print(" ", end="")
+        # Imprimir asteriscos
+        for k in range(2 * i + 1):
+            print("*", end="")
+        # Nueva línea después de cada fila
+        print()
+
+# Solicitar al usuario la cantidad de filas
+n = int(input("Ingresa la cantidad de filas para la pirámide: "))
+imprimir_piramide(n)
